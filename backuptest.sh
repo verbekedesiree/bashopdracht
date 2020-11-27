f2c5351e23838c7dce7d5fc2618c5b4c1344fbf3
Désirée verbeke <desiree.verbeke@student.howest.be>
2020-11-27 17:49:48 +0100

    'testbackup'

---
+++ backuptest.sh
@@ -0,0 +1,9 @@
+tar -czvf /home/user/backups/backupfile.tar /home/user/testfiles/
+
+if [ ! -s "/etc/cron.weekly/make_backup"]
+		then 
+			sudo cp make_backup.sh /etc/cron.weekly/make_backup	
+			echo "the backup schedule has been set to run weekly"
+			echo "the exact run time is in the /etc/crontab file"
+			exit 1
+fi
--- make_backup.sh
+++ make_backup.sh
@@ -60,6 +60,17 @@ function perform_backup {
 		date >> $LOG_LOC
 }
 
+tar -czvf /home/user/backups/backupfile.tar /home/user/testfiles/
+
+if [ ! -s "/etc/cron.weekly/make_backup"]
+		then 
+			sudo cp make_backup.sh /etc/cron.weekly/make_backup	
+			echo "the backup schedule has been set to run weekly"
+			echo "the exact run time is in the /etc/crontab file"
+			exit 1
+fi
+
+
 ##end function
 check_dir_loc
 check_backup_loc
