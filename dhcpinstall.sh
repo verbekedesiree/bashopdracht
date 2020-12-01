a3d193d1d95c2a4b4993d85a0912bcbe85e32a8c
Désirée verbeke <desiree.verbeke@student.howest.be>
2020-12-01 01:03:52 +0100

    'dhcpfault'

--- dhcpinstall.sh
+++ dhcpinstall.sh
@@ -2,6 +2,6 @@ sudo apt install -y isc-dhcp-server
 
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf_backup
 
-cp installdhcp.txt > /etc/dhcp/dhcpd.conf
+cp ./installdhcp.txt > /etc/dhcp/dhcpd.conf
 
 systemctl restart isc-dhcp-server
