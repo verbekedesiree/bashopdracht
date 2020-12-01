5b858e7e71c15b34a28b248aa05768709d4d1be9
Désirée verbeke <desiree.verbeke@student.howest.be>
2020-12-01 18:18:44 +0100

    'weeklybackup_change'

--- 2.backup.sh
+++ 2.backup.sh
@@ -4,8 +4,8 @@ tar -czvf /home/user/backups/backupfile.tar /home/user/testfiles/
 
 if [ ! -s "/etc/cron.weekly/make_backup" ];
 		then 
-			sudo cp make_backup.sh /etc/cron.d/make_backup.sh
-			echo "de file is in de cron.d gestopt."
+			sudo cp make_backup.sh /etc/cron.weekly/make_backup.sh
+			echo "de file is in de cron.weekly gestopt."
 			echo "de exacte tijd kan bekeken worden in de  /etc/crontab file"
 			exit 1
 fi
