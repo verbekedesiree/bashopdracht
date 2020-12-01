c9bc9cb8e11e03c8b75281b88ed17c6bbd956c17
Désirée verbeke <desiree.verbeke@student.howest.be>
2020-12-01 12:30:22 +0100

    dhcp_foutje

--- dhcpinstall.sh
+++ dhcpinstall.sh
@@ -2,6 +2,14 @@ sudo apt install -y isc-dhcp-server
 
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf_backup
 
-cp ./installdhcp.txt > /etc/dhcp/dhcpd.conf
+cp /home/user/bashopdracht/installdhcp.txt /etc/dhcp/dhcpd.conf
+
+
+cp /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server_backup
+
+echo '
+INTERFACESv4="ens192"
+#INTERFACESv6=""
+' >> /etc/default/isc-dhcp-server
 
 systemctl restart isc-dhcp-server
